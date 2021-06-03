d29d620cf06456f602886e89bc08e0bed01670a8
s1123069 <s1123069@student.hsleiden.nl>
2021-06-03 19:24:15 +0200

    filterfunctie gemaakt

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,167 @@
 
 let list_of_product = document.getElementsByTagName("li");
 
-for (let i = 0; i < 2; i++) {
-    console.log(list_of_product);   
+let checkbox_keuken = document.getElementById("keuken");
+let checkbox_gereedschap = document.getElementById("gereedschap");
+let checkbox_kantoor = document.getElementById("kantoor");
+let checkbox_elektronica = document.getElementById("elektronica");
+let checkbox_hobby = document.getElementById("hobby");
+let checkbox_reizen = document.getElementById("reizen");
+let checkbox_sport = document.getElementById("sport");
+let checkbox_overig = document.getElementById("overig");
+
+
+checkbox_keuken.checked = true;
+checkbox_gereedschap.checked = true;
+checkbox_kantoor.checked = true;
+checkbox_elektronica.checked = true;
+checkbox_hobby.checked = true;
+checkbox_reizen.checked = true;
+checkbox_sport.checked = true;
+checkbox_overig.checked = true;
+
+
+for(let i = 0; i < list_of_product.length; i++){
+    list_of_product[i].style.display = "block";
 }
 
+// console.log(list_of_product[0].dataset);
 
-// console.log(list_of_product);
+checkbox_keuken.addEventListener('change', function(){
+    if(checkbox_keuken.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Keuken'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Keuken'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+
+checkbox_gereedschap.addEventListener('change', function(){
+    if(checkbox_gereedschap.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Gereedschap'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Gereedschap'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+
+checkbox_kantoor.addEventListener('change', function(){
+    if(checkbox_kantoor.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Kantoor'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Kantoor'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+
+checkbox_elektronica.addEventListener('change', function(){
+    if(checkbox_elektronica.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Elektronica'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Elektronica'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+checkbox_hobby.addEventListener('change', function(){
+    if(checkbox_hobby.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Hobby'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Hobby'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+checkbox_reizen.addEventListener('change', function(){
+    if(checkbox_reizen.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Reizen'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Reizen'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+checkbox_sport.addEventListener('change', function(){
+    if(checkbox_sport.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Sport'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Sport'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
+
+checkbox_overig.addEventListener('change', function(){
+    if(checkbox_overig.checked){
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Overig'){
+                list_of_product[i].style.display = 'block';
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < list_of_product.length; i++){
+            if (list_of_product[i].dataset.kindOfProduct == 'Overig'){
+                list_of_product[i].style.display = 'none';
+            }
+        }
+    }
+});
--- resources/views/time2share/index.blade.php
+++ resources/views/time2share/index.blade.php
@@ -4,7 +4,59 @@
     Producten
 @endsection
 
+
 @section('content')
+
+    <details class="filter">
+        <summary>Filtreer</summary>
+            <section class="filter__grid">
+                
+                <section class="filter__grid__items">
+                    <label class="" for="keuken">Keuken</label>
+                    <input class="" id="keuken" name="keuken" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="gereedschap">Gereedschap</label>
+                    <input class="" id="gereedschap" name="gereedschap" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="kantoor">Kantoor</label>
+                    <input class="" id="kantoor" name="kantoor" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="elektronica">Elektronica</label>
+                    <input class="" id="elektronica" name="elektronica" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="hobby">Hobby</label>
+                    <input class="" id="hobby" name="hobby" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="reizen">Reizen</label>
+                    <input class="" id="reizen" name="reizen" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="sport">Sport</label>
+                    <input class="" id="sport" name="sport" type="checkbox"/>
+                </section>
+
+                <section class="filter__grid__items">
+                    <label class="" for="overig">Overig</label>
+                    <input class="" id="overig" name="overig" type="checkbox"/>
+                </section>
+
+                </section>
+    </details>
+
+    
+
+
     <ul class="u-grid-12 u-grid-gap-2">
     @foreach($product as $product)
     <li class="a-popup u-list-style-none gridCard" data-kind-of-product="{{$product->kind_of_product}}">
